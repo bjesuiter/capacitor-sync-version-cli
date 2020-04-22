a34fd2c464288e2ce736c395f870f4be90144258
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2020-04-22 17:16:19 +0200

    update docs in shared.js

--- src/modules/shared.js
+++ src/modules/shared.js
@@ -1,5 +1,9 @@
-export function splitVersionIntoParts(newVersionString) {
-	const [versionMajor, versionMinor, rest] = newVersionString.split('.');
+/**
+ * Extracts the parts of a npm version string
+ * @param {*} npmVersionString A npm version string of the format major.minor.patch-prerelease
+ */
+export function splitVersionIntoParts(npmVersionString) {
+	const [versionMajor, versionMinor, rest] = npmVersionString.split('.');
 	const [versionPatch, versionPrerelease] = rest.split('-');
 
 	return {
