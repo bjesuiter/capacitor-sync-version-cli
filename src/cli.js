45e9776103f47993b16224318cb4027123cf1a9e
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2020-04-22 13:18:07 +0200

    add logdown package and add error handling to cli

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,8 @@
-import {Command} from 'commander';
+import {Command, cli} from 'commander';
 import projectVersion from 'project-version';
+import logdown from 'logdown';
+const logger = logdown('Cap Sync Version');
+logger.state = {isEnabled: true};
 
 const cli = new Command();
 cli.version(projectVersion, '-v, --version')
@@ -20,4 +23,18 @@ cli.on('--help', () => {
             Default Behavior: syncs the package version to android and ios, if available
         `
 	);
+});
+
+cli.parse(process.argv);
+
+if (process.argv.slice(2).length === 0) {
+	// Display the help text in red on the console
+	cli.outputHelp();
+	cli.exit(0);
+}
+
+async function main(cli) {}
+
+main(cli).catch(error => {
+	logger.error('Execution Error:', error);
 });
