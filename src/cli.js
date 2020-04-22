8fced186f1c10c024e4d02bde862ac6923f74bad
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2020-04-22 16:34:24 +0200

    improve options handling and logging

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,7 @@ import {Command} from 'commander';
 import projectVersion from 'project-version';
 import readPkg from 'read-pkg';
 import {updateAndroidVersion} from './modules/android';
+import {updateIosVersion} from './modules/ios';
 
 import logdown from 'logdown';
 const logger = logdown('Cap Sync Version');
@@ -9,14 +10,22 @@ logger.state = {isEnabled: true};
 
 const cli = new Command();
 cli.version(projectVersion, '-v, --version')
-	.option('-a, --android', 'Sync package version to android', true)
+	.option(
+		'-a, --android',
+		'Sync package version to android. It will not update iOS, unless --ios is specified.',
+		false
+	)
 	.option(
 		'-p, --android-allow-prerelease',
 		'Allows prerelease versions for android. Warning: iOS does not support prerelease versions! ' +
 			'So If you create a npm package prerelease version and allow prereleases for android, the ios version will simply be the version without prerelease number!',
 		false
 	)
-	.option('-i, --ios', 'Sync package version to ios', true);
+	.option(
+		'-i, --ios',
+		'Sync package version to ios.  It will not update Android, unless --android is specified.',
+		false
+	);
 
 cli.on('--help', () => {
 	console.log(
@@ -28,14 +37,23 @@ cli.on('--help', () => {
 	);
 });
 
+cli.parse(process.argv);
+
+if (process.argv.slice(2).length === 0) {
+	// Set android and ios flags to true when no params are given.
+	// This allows
+	cli.android = true;
+	cli.ios = true;
+}
+
 async function main(cli) {
 	const projectPackageJson = await readPkg();
 	const packageVersion = projectPackageJson.version;
 
-	logger.log('Package Version', packageVersion);
-	logger.log('CLI', cli);
+	logger.log('Updating capacitor project versions to: ', packageVersion);
 
 	await updateAndroidVersion(packageVersion, cli.androidAllowPrerelease);
+	await updateIosVersion(packageVersion);
 }
 
 main(cli).catch(error => {
