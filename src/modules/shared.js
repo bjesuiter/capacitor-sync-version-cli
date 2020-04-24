282f96d31e4960d14a949f5850e8f074310dec57
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2020-04-24 20:04:04 +0200

    convert all numbers in splitVersionIntoParts to ints instead of leaving them as strings

--- src/modules/shared.js
+++ src/modules/shared.js
@@ -3,8 +3,17 @@
  * @param {*} npmVersionString A npm version string of the format major.minor.patch-prerelease
  */
 export function splitVersionIntoParts(npmVersionString) {
-	const [versionMajor, versionMinor, rest] = npmVersionString.split('.');
-	const [versionPatch, versionPrerelease] = rest.split('-');
+	let [versionMajor, versionMinor, rest] = npmVersionString.split('.');
+	let [versionPatch, versionPrerelease] = rest.split('-');
+
+	// Convert all to int
+	versionMajor = Number.parseInt(versionMajor, 10);
+	versionMinor = Number.parseInt(versionMinor, 10);
+	versionPatch = Number.parseInt(versionPatch, 10);
+
+	if (versionPrerelease) {
+		versionPrerelease = Number.parseInt(versionPrerelease, 10);
+	}
 
 	return {
 		versionMajor,
