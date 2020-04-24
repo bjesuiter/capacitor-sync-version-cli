398476f952d1278a32966a078088b28549b3dc82
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2020-04-24 19:51:23 +0200

    add tests for correct android version code gen + that it throws on versions with prerelease number

--- src/modules/android.test.js
+++ src/modules/android.test.js
@@ -1,7 +1,17 @@
 import test from 'ava';
 import {buildAndroidVersionCode} from './android';
 
-test('buildAndroidVersionCode: 0.0.1 => 1', t => {
-	const versionCode = buildAndroidVersionCode('0.0.1');
-	t.is(versionCode, 1);
+function versionCodeGenMacro(t, input, expected) {
+	const versionCode = buildAndroidVersionCode(input);
+	t.is(versionCode, expected);
+}
+
+versionCodeGenMacro.title = (providedTitle, input, expected) => `${providedTitle} ${input} => ${expected}`.trim();
+
+test('Function buildAndroidVersionCode', versionCodeGenMacro, '0.0.1', 1);
+test('Function buildAndroidVersionCode', versionCodeGenMacro, '0.1.0', 1000);
+test('Function buildAndroidVersionCode', versionCodeGenMacro, '1.0.0', 1000000);
+
+test('Function buildAndroidVersionCode throws on prerelease versions', t => {
+	t.throws(() => buildAndroidVersionCode('2.2.0-1'));
 });
