fe67989e91d2b9ef5d33fe742795b5a1941fb941
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2020-04-24 20:05:18 +0200

    add tests for version code generation

--- src/modules/android.test.js
+++ src/modules/android.test.js
@@ -1,17 +1,36 @@
 import test from 'ava';
 import {buildAndroidVersionCode} from './android';
 
+// General Helper Functions
+const macroTitleFunction = (providedTitle, input, expected) => `${providedTitle} ${input} => ${expected}`.trim();
+
+// First Test Area => good-path tests for buildAndroidVersionCode
 function versionCodeGenMacro(t, input, expected) {
 	const versionCode = buildAndroidVersionCode(input);
 	t.is(versionCode, expected);
 }
 
-versionCodeGenMacro.title = (providedTitle, input, expected) => `${providedTitle} ${input} => ${expected}`.trim();
+versionCodeGenMacro.title = macroTitleFunction;
 
 test('Function buildAndroidVersionCode', versionCodeGenMacro, '0.0.1', 1);
 test('Function buildAndroidVersionCode', versionCodeGenMacro, '0.1.0', 1000);
 test('Function buildAndroidVersionCode', versionCodeGenMacro, '1.0.0', 1000000);
 
-test('Function buildAndroidVersionCode throws on prerelease versions', t => {
-	t.throws(() => buildAndroidVersionCode('2.2.0-1'));
-});
+// Second Test Area => throwing tests for buildAndroidVersionCode
+
+function versionCodeGenThrowing(t, input) {
+	t.throws(() => buildAndroidVersionCode(input));
+}
+
+function versionCodeGenNotThrowing(t, input) {
+	t.notThrows(() => buildAndroidVersionCode(input));
+}
+
+test('Function buildAndroidVersionCode throws on prerelease versions', versionCodeGenThrowing, '2.2.0-1');
+test('Function buildAndroidVersionCode throws on major version too high', versionCodeGenThrowing, '2148.0.0');
+test('Function buildAndroidVersionCode throws on minor version too high', versionCodeGenThrowing, '2.1000.0');
+test('Function buildAndroidVersionCode throws on patch version too high', versionCodeGenThrowing, '2.0.1000');
+
+test('Function buildAndroidVersionCode throws not major version on edge', versionCodeGenNotThrowing, '2147.0.0');
+test('Function buildAndroidVersionCode throws not minor version on edge', versionCodeGenNotThrowing, '2.999.0');
+test('Function buildAndroidVersionCode throws not patch version on edge', versionCodeGenNotThrowing, '2.0.999');
